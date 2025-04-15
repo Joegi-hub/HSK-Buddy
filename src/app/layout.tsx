967334748a2b528a6ec86b1d0acc230852ce3056
Comte <cjzfam7@gmail.com>
2025-04-15 05:34:45 +0000

    이제 지구본을 누르고, 해당 언어를 누르면 프로그램 언어가 선택한 언어로 변경될 수 있게 수정해줘요

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type {Metadata} from 'next';
 import {Geist, Geist_Mono} from 'next/font/google';
 import './globals.css';
@@ -36,4 +35,3 @@ export default function RootLayout({
     </html>
   );
 }
-
